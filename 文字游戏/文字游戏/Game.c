54d5014dcaa1dc679c81fc754ac69510eb1ef433
be-doing <1782904648@qq.com>
2019-01-28 22:23:59 +0800

    待续

--- 文字游戏/文字游戏/Game.c
+++ 文字游戏/文字游戏/Game.c
@@ -2,28 +2,62 @@
 #include<stdio.h>
 #include<string.h>
 #include<Windows.h>
+#include<time.h>
+char name[1024] = { 0 };
+char* sex[1];
+char name2[1024] = { 0 };
+//
+void Choice()
+{
+	getchar();
+again2:
+		printf("为了引导正确的取向观，请选择您的对象：\n");
+		printf("A—女朋友，B—男朋友");
+		printf("您的选择是：");
+		char choice = 0;
+		scanf("%c", &choice);
+		if (choice == 'A')
+		{
+			if (sex[0] == "女")
+			{
+				printf("为构建和谐的社会主义核心价值观，请重新选择\n");
+				goto again2;
+			}
+			printf("请为您的女朋友取一个名字\n");
+		}
+		else if (choice == 'B')
+		{
+			if (sex[0] == "男")
+			{
+				printf("为构建和谐的社会主义核心价值观，请重新选择\n");
+				goto again2;
+			}
+			printf("请为您的男朋友取一个名字\n");
+		}
+		else if (choice != 'A' || choice != 'B')
+		{
+			printf("输入非法！请重新输入。\n");
+			getchar();
+			goto again2;
+		}
+}
+//登录
 void Log_in()
 {
-	printf("正在登录……0%%\n");
-	Sleep(1111);
-	printf("正在登录……10%%\n");
-	Sleep(1111);
-	printf("正在登录……25%%\n");
-	Sleep(1111);
-	printf("正在登录……50%%\n");
-	Sleep(1111);
-	printf("正在登录……66%%\n");
-	Sleep(1111);
-	printf("正在登录……79%%\n");
-	Sleep(1111);
-	printf("正在登录……90%%\n");
-	Sleep(1111);
-	printf("正在登录……100%%\n");
+	int log = 0;
+	srand((int)time(0));
+	for (int i = 0; i < 9; ++i)
+	{
+		printf("%s正在登录……%d%%\n",name, log);
+		log = log + rand() % 20;
+		Sleep(1111);
+	}
+	printf("%s正在登录……100%%\n",name);
+	Choice();
 }
+//界面
 void Game()
 {
-	char name[1024] = { 0 };
-	
 	getchar();
 	while (1)
 	{
@@ -36,10 +70,31 @@ void Game()
 		}
 		else
 		{
-			break;
+			printf("%s您好，请选择您的性别：",name);
+			printf("A—男\tB—女\n");
+			printf("您的选择是：");
+			char choice = getchar();
+			if (choice == 'A')
+			{
+				sex[0] = "男";
+				break;
+			}
+			else if (choice == 'B')
+			{
+				sex[0] = "女";
+				
+				break;
+			}
+			else
+			{
+				getchar();
+				memset(name,0,1023);
+				printf("非法输入，请重新输入！\n");
+			}
 		}
 	}
-	printf("您的ID是%s\n",name);
+	printf("您的角色是：%s，您的性别是：%s\n",name,sex[0]);
+	//登录
 	Log_in();
 }
 void Start()
@@ -47,23 +102,21 @@ void Start()
 	char choice = 0;
 	printf("欢迎进入模拟情侣游戏！\n");
 again:
-	printf("输入 1 进入游戏，输入 0 退出游戏。请输入您的选择：");
-	choice = getchar();
-	if (choice)
 	{
-		if (choice != '1' && choice != '0')
-		{
-			printf("输入非法！\n");
-			goto again;
-		}
-		else
+		printf("输入 1 进入游戏，输入 0 退出游戏。请输入您的选择：");
+		scanf("%c", &choice);
+		if (choice == '1')
 		{
 			Game();
 		}
-		
+		else if (choice == '0')
+		{
+			return;
+		}
+		else
+		{
+			printf("非法输入，请重新输入。");
+			goto again;
+		}
 	}
-	else
-	{
-		return;
-	}
-} 
+}
