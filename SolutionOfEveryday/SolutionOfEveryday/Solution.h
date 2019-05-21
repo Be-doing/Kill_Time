4455cca8253094c173622334b4512c3acbe03d2b
be-doing <1782904648@qq.com>
2019-05-21 23:53:45 +0800

    待续

--- SolutionOfEveryday/SolutionOfEveryday/Solution.h
+++ SolutionOfEveryday/SolutionOfEveryday/Solution.h
@@ -1,12 +1,31 @@
 #pragma once
-
+#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #include<vector>
+#include<string>
 using namespace std;
 
-class Solution2
+//牛牛举办了一次编程比赛, 参加比赛的有3*n个选手, 每个选手都有一个水平值a_i.现在要将这些选手进行组队, 一共组成n个队伍, 即每个队伍3人.牛牛发现队伍的水平值等于该队伍队员中第二高水平值。
+//例如 :
+//一个队伍三个队员的水平值分别是3, 3, 3.那么队伍的水平值是3
+//一个队伍三个队员的水平值分别是3, 2, 3.那么队伍的水平值是3
+//一个队伍三个队员的水平值分别是1, 5, 2.那么队伍的水平值是2
+//为了让比赛更有看点, 牛牛想安排队伍使所有队伍的水平值总和最大。
+//如样例所示 :
+//如果牛牛把6个队员划分到两个队伍
+//如果方案为 :
+//team1: {1, 2, 5}, team2 : {5, 5, 8}, 这时候水平值总和为7.
+//	而如果方案为 :
+//	team1 : {2, 5, 8}, team2 : {1, 5, 5}, 这时候水平值总和为10.
+//	没有比总和为10更大的方案, 所以输出10.
+#define TEAMNUMMAX 500000
+#define PERSONLEVEL 1000000000
+#include<algorithm>
+class Day1
 {
 public:
+	//思路，先将队伍的人数进行排序，选择最大的和第二大的数，加最小的数。同理一次选择
+	//最后加上每个队伍中间的数。
 	void Sort(vector<int>& v, size_t size)
 	{
 		for (size_t i = 1; i < size; ++i)
@@ -20,39 +39,132 @@ public:
 			}
 		}
 	}
-	bool Programe2()
+	bool SolutionOne()
 	{
-		int teamNum = 0;
-		int sum = 0;
+		int teamNum = 2;
+
 		cout << "请输入队伍数量: ";
 		cin >> teamNum;
-		if (teamNum < 1 && teamNum > 10 ^ 5)
+		if (teamNum < 1 && teamNum > TEAMNUMMAX)
 		{
 			cout << "输入非法" << endl;
 			return false;
 		}
-		vector<int> people;
-		people.resize(3 * teamNum);
+		vector<int> personNum;
+		personNum.resize(3 * teamNum);
 		cout << "请输入选手顺平值：" << endl;
 		for (int i = 0; i < 3 * teamNum; ++i)//每队3人
 		{
 			cout << "选手[ " << i << " ] :";
-			cin >> people[i];
-			if (people[i] < 1 && people[i] > 10 ^ 9)
+			cin >> personNum[i];
+			if (personNum[i] < 1 && personNum[i] > PERSONLEVEL)
 			{
 				cout << "输入非法" << endl;
 				return false;
 			}
 		}
-		size_t size = people.size();
-		Sort(people, size);
-
-		int mid = teamNum;
-		for (int i = 0; i < teamNum; ++i)
+		size_t size = personNum.size();
+		//Sort(personNum, size);
+		sort(personNum.begin(), personNum.end());//递增排序
+		long long sum = 0;
+		//int mid = teamNum;
+		for (size_t mid = teamNum; mid <= size - 2; mid += 2)
 		{
-			sum += people[mid];
-			++mid;
+			sum += personNum[mid];
 		}
 		cout << sum << endl;
+		return true;
+	}
+
+	//输入两个字符串，从第一字符串中删除第二个字符串中所有的字符。
+	//例如，输入”They are students.”和”aeiou”，则删除之后的第一个字符串变成”Thy r stdnts.”
+	void SolutionTwo()
+	{
+		string src_;
+		string des_;
+		getline(cin, src_);
+		getline(cin, des_);
+
+		int hash[256] = { 0 };
+		int srcLen_ = src_.size();
+		int desLen_ = des_.size();
+
+		//ascii码表对应
+		for (int i = 0; i < desLen_; ++i)
+		{
+			hash[des_[i]]++;
+		}
+		string res;
+		for (int i = 0; i < srcLen_; ++i)
+		{
+			if (hash[src_[i]] == 0)
+			{
+				res += src_[i];
+			}
+		}
+		cout << res;
+	}
+};
+class Solution2
+{
+public:
+	int Programe1()
+	{
+		int num = 0;
+		printf("数字个数（数组长度）：");
+		scanf("%d", &num);
+		if (num < 1 || num > 100000)
+		{
+			printf("输入非法\n");
+			return -1;
+		}
+
+		//int num = 6;
+		int* arr = new int[num];
+		printf("输入数据（空格隔开）：");
+		for (int i = 0; i < num; ++i)
+		{
+			scanf("%d", &arr[i]);//1 2 3 2 2 1
+			if (arr[i] < 1 || arr[i] > 1000000000)
+			{
+				printf("输入非法\n");
+				return -1;
+			}
+		}
+
+		//int arr[6] = { 1,2,3,2,2, 1};
+		//只有一个数或者两个数的数组，要么递增要么递减
+		if (num == 1 || num == 2)
+		{
+			return 1;
+		}
+
+
+		int flag = 1;		//标记每个子序列是递增的还是递减的
+		int count = 1;
+		//除上面的情况外,数组的长度是大于2的。
+		//如果一个数大于后面的数，flag置1，向后执行，否则++cout；flag置为2，向后执行否则++count
+		if (arr[0] < arr[1])
+		{
+			flag = 1;
+		}
+		else
+		{
+			flag = 2;
+		}
+		for (int i = 1; i < num; ++i)
+		{
+			if (flag == 2 && arr[i] < arr[i + 1])
+			{
+				flag = 1;
+				++count;
+			}
+			else if (flag == 1 && arr[i] > arr[i + 1])
+			{
+				flag = 2;
+				++count;
+			}
+		}
+		return count;
 	}
 };
--- SolutionOfEveryday/SolutionOfEveryday/源.cpp
+++ SolutionOfEveryday/SolutionOfEveryday/源.cpp
@@ -1,14 +1,83 @@
 #include"Solution.h"
 
-void test2()
+//int test2()
+//{
+//	Solution2 s;
+//	return s.Programe1();
+//}
+//
+//
+//int main()
+//{
+//	int res = test2();
+//	printf("%d\n", res);
+//	system("pause");//1 2 3 2 2 1 2 3 4 6 9
+//	return 0;
+//}'
+void test1()
 {
-	Solution2 s;
-	s.Programe2();
-}
+	Day1 solution;
+	//solution.SolutionOne();
 
+	solution.SolutionTwo();
+	
+}
 int main()
 {
-	test2();
-	system("pause");//52855 == 10  5 9 6 7 4 1 3 6 8
+	test1();
+	system("pause");
 	return 0;
 }
+
+
+//#include<iostream>
+//#include<string.h>
+//#include<vector>
+//
+//using namespace std;
+//int leng(char* str) {
+//	int len = 0;
+//	while (str[len] != '\0') {
+//		++len;
+//	}
+//	return len;
+//}
+//void swap_one(char* str, int star, int end) {
+//
+//	while (star <= end) {
+//		char temp = str[star];
+//		str[star] = str[end];
+//		str[end] = temp;
+//		++star;
+//		--end;
+//	}
+//}
+//
+//void swap_two(char* str) {
+//	int temp = 0;
+//	int start = 0;
+//	int end = leng(str) - 1;
+//	swap_one(str, start, end);
+//	printf("第一次翻转后的字符串：%s\n", str);
+//	while (str[temp] != '\0') {
+//		start = temp;
+//		while ((str[temp] != '\0') && (str[temp] != ' ')) {
+//			temp++;
+//		}
+//		end = temp - 1;
+//		swap_one(str, start, end);
+//		if (str[temp] != '\0') {
+//			temp++;
+//		}
+//	}
+//	printf("最后一次翻转后的字符串为：%s\n", str);
+//}
+//int main() {
+//	char orig[100];
+//	cin.getline(orig, 100);
+//	int end = leng(orig) - 1;
+//	int star = 0;
+//	swap_two(orig);
+//	system("pause");
+//	return 0;
+//}
